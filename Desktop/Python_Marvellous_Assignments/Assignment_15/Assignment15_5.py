b50c60795c656b649e0461c960fc25cbe02310c4
aayushmusale <160577172+aayushmusale@users.noreply.github.com>
2025-06-11 13:53:54 +0530

    Uploaded Assignment15_5.py

--- Desktop/Python_Marvellous_Assignments/Assignment_15/Assignment15_5.py
+++ Desktop/Python_Marvellous_Assignments/Assignment_15/Assignment15_5.py
@@ -14,17 +14,26 @@ def CompareFiles(Fname, keyword):
         print("Path is valid but it is not a file : ", Fname)       
         exit()
     
-    fobj = open(Fname, 'r')
-    data = fobj.read()
-
+    fobj = open(Fname, 'r')       
+    lineList = fobj.readlines()
+    Frequency = 0
     
+    for line in lineList:
+        wordList = line.split(" ")
+        # print(wordList)
+        for word in wordList:
+            if(word == keyword or word == keyword+"\n"):          #taking the word in between the lines and also at the End of the Lines
+                Frequency += 1
 
+    return Frequency 
+        
+    
 
 def main():
     if(len(sys.argv) == 2):
         if((sys.argv[1]) == "--h" or (sys.argv[1]) == "--H"):
             print("--u : Usage")
-            print("ScriptName.py Argument1 Argument2")
+            print("ScriptName.py FileName.txt Keyword")
             
         elif((sys.argv[1]) == "--u" or (sys.argv[1]) == "--U"):
             print("Usage for copying the contents of the file using Command Line Arguments")
@@ -35,11 +44,15 @@ def main():
             print('--u : Usage')
     elif(len(sys.argv) == 3):
         ret = CompareFiles(sys.argv[1], sys.argv[2]) 
-        print("The frequency of ", sys.argv[2], " in ", sys.argv[1], " is ", ret)     
+        print("The frequency of ", sys.argv[2], " in ", sys.argv[1], " is ", ret)   
+
     else:
         print("Invalid number of Command Line Arguements")
         print("use --h for help")
         print("use --u for usage")
+
+
+
 
 
 if __name__ == "__main__":
